7a71ef88fbe2c22da03c6b28d7d56e8a5607cc6c
Jonathan Bereyziat <bereyziat.developer@gmail.com>
2022-07-18 09:48:06 +0200

    Initial Commit

---
+++ SimpleOrion/SimpleOrionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleOrionApp.swift
+//  SimpleOrion
+//
+//  Created by Jonathan Bereyziat on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleOrionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
